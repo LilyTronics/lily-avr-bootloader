e0c945ba565cee9efa1c155035dfb11bf82b6fdc
Danny <danny@lilytronics.nl>
2023-11-21 20:06:24 +0100

    add info about setting the correct boot flash size and start address

--- firmware/general_bootloader/main.cpp
+++ firmware/general_bootloader/main.cpp
@@ -1,5 +1,15 @@
 /*
  * General bootloader based on Arduino Uno platform with ATmega238 running at 16MHz.
+ *
+ * With fuses set the boot flash size to 1024 words (start address 0x3c00).
+ * In the project properties, go to tool chain. Under AVR/GNU linker, go to memory settings.
+ * Add the flash segment: .text=0x3c00.
+ * This should place the boot loader code in the boot flash section.
+ * Note that the compiler is not checking if the boot loader is too big.
+ * Check in the output: Program Memory Usage : xx bytes
+ * Note this is in bytes. The size of the boot section is in words (1024 words = 2048 bytes).
+ * You can also check the .hex output file. The code should start at address 0x7800 (byte address).
+ *
  */
 
 #include <avr/io.h>
