2f2dbd353ea90f1e134504969a647c82d79762ca
Danny <danny@lilytronics.nl>
2023-12-10 19:12:26 +0100

    add model for listing available serial ports

---
+++ application_software/src/models/list_serial_ports.py
@@ -0,0 +1,52 @@
+"""
+Lists all the available serial ports on the system.
+"""
+
+import serial
+import threading
+import time
+
+
+def get_available_serial_ports():
+    ports = []
+    threads = []
+    lock = threading.RLock()
+
+    for i in range(1, 256):
+        port = 'COM%d' % i
+        t = threading.Thread(target=_check_serial_port, args=(lock, port, ports))
+        t.daemon = True
+        t.start()
+        threads.append(t)
+
+    while True in list(map(lambda x: x.is_alive(), threads)):
+        time.sleep(0.1)
+
+    if len(ports) == 0:
+        ports.append('no ports')
+
+    return ports
+
+
+def _check_serial_port(lock_object, port_name, port_list):
+    p = None
+    try:
+        p = serial.Serial(port_name)
+        lock_object.acquire()
+        try:
+            port_list.append(port_name)
+        finally:
+            lock_object.release()
+    except (Exception, ):
+        pass
+
+    if p is not None:
+        p.close()
+
+
+if __name__ == '__main__':
+
+    _ports = get_available_serial_ports()
+    print('\n', _ports)
+
+    print('\nDone')
