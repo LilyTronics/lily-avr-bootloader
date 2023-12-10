973935b3402ca53d4820e2bed9ee2b15b9b63a85
Danny <danny@lilytronics.nl>
2023-12-10 18:59:31 +0100

    add application init data

---
+++ application_software/src/app_init.py
@@ -0,0 +1,13 @@
+"""
+Application init file.
+"""
+
+import os
+
+
+APP_NAME = 'Lily AVR proggrammer'
+EXE_NAME = 'LilyAvrProg'
+VERSION = '1.0'
+COMPANY = 'LilyTronics'
+
+USER_FOLDER = os.path.join(os.path.expanduser('~'))
