40ebd44bc40ad63da768599ac8980ab6dd4e4613
Danny <danny@lilytronics.nl>
2023-12-11 21:44:20 +0100

    ﻿fix correct size of bootloader

---
+++ firmware/general_bootloader/device_info.h
@@ -0,0 +1,21 @@
+/*
+ * device_info.h
+ *
+ * This contains all info of the device.
+ */
+
+
+#ifndef DEVICE_INFO_H_
+#define DEVICE_INFO_H_
+
+#define SYS_CLOCK               16000000
+#define BAUD_RATE               115200
+
+#define FLASH_SIZE              32          // KB (32 * 1024)
+#define PAGE_SIZE               128         // Bytes
+#define BOOT_SIZE               4096        // Bytes
+
+const char DEVICE_NAME[] = "ATmega328P";
+const char MODULE_NAME[] = "Arduino Uno R3";
+
+#endif /* DEVICE_INFO_H_ */
