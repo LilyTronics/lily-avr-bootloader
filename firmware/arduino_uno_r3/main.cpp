a09cbd8be4d4321d8bd325f39312597b0f1a4c5c
Danny <danny@lilytronics.nl>
2023-12-12 14:20:01 +0100

    replaced the development project with Arduino Uno specific project as demonstrator

---
+++ firmware/arduino_uno_r3/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * Bootloader for Arduino Uno R3
+ */ 
+
+#include <avr/pgmspace.h>
+#include "uart.h"
+#include "bootloader.h"
+
+
+#define SYS_CLOCK               16000000
+#define BAUD_RATE               115200
+
+const char DEVICE_NAME[] PROGMEM = "ATmega328P";
+const char MODULE_NAME[] PROGMEM = "Arduino Uno R3";
+
+
+int main(void) {
+	Uart uart(SYS_CLOCK, BAUD_RATE);
+
+	Bootloader bootloader(
+	SYS_CLOCK,
+	PORTB5,
+	&DDRB,
+	&PORTB,
+	&uart,
+	DEVICE_NAME,
+	MODULE_NAME
+	);
+
+	while (1) {
+		bootloader.process_events();
+	}
+}
