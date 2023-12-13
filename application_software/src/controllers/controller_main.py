b7c2c1f010700c3a81018892a8c73c7f32cfb01b
Danny <danny@lilytronics.nl>
2023-12-13 20:39:17 +0100

    add read data from FLASH and write to file

--- application_software/src/controllers/controller_main.py
+++ application_software/src/controllers/controller_main.py
@@ -8,6 +8,7 @@ import wx
 
 from src.models.application_settings import ApplicationSettings
 from src.models.boot_loader import BootLoader
+from src.models.hex_file import write_hex_file
 from src.models.list_serial_ports import get_available_serial_ports
 from src.models.list_serial_ports import get_available_baud_rates
 from src.views.view_main import ViewMain
@@ -41,6 +42,7 @@ class ControllerMain(object):
         self._view.set_flash_verify(self._application_settings.get_flash_verify())
 
         self._view.Bind(wx.EVT_BUTTON, self._on_connect, id=self._view.ID_BUTTON_CONNECT)
+        self._view.Bind(wx.EVT_BUTTON, self._on_flash_read, id=self._view.ID_BUTTON_READ)
         self._view.Bind(wx.EVT_CLOSE, self._on_view_close)
 
     ###########
@@ -68,12 +70,74 @@ class ControllerMain(object):
                 dlg.ShowModal()
             self._boot_loader = None
 
+    def _read_flash(self):
+        title = 'Read FLASH'
+
+        with wx.FileDialog(self._view, title, style=wx.FD_SAVE) as dlg:
+            if dlg.ShowModal() == wx.ID_OK:
+                filename = dlg.GetPath()
+                if not filename.endswith('.hex'):
+                    filename += '.hex'
+
+                if self._boot_loader is None:
+                    self._connect()
+                if self._boot_loader is None:
+                    return
+
+                flash_size = self._boot_loader.get_flash_size() * 1024
+                page_size = self._boot_loader.get_page_size()
+                n_pages = int(flash_size / page_size)
+
+                address = 0
+                flash_data = b''
+                self._view.set_progress(0, n_pages)
+                try:
+                    for i in range(1, n_pages + 1):
+                        self._view.set_status_label('Reading page {} of {}'.format(i, n_pages))
+                        self._boot_loader.set_page_address(address)
+                        flash_data += self._boot_loader.read_page()
+                        address += page_size
+                        self._view.set_progress(i)
+                        i += 1
+                except Exception as e:
+                    message = 'Error reading data from FLASH:\n{}'.format(e)
+                    with wx.MessageDialog(self._view, message, title, wx.ICON_EXCLAMATION) as dlg:
+                        dlg.ShowModal()
+                    self._view.set_progress(0)
+                    self._view.set_status_label('Error reading FLASH')
+                    return
+
+                try:
+                    write_hex_file(filename, flash_data, 0)
+                    if len(filename) > 70:
+                        parts = filename.split(os.sep)
+                        first_part = os.sep.join(parts[0:2]) + os.sep
+                        last_part = ''
+                        i = 2
+                        while i < len(parts):
+                            last_part = os.sep + os.sep.join(parts[i:])
+                            if len(last_part) + len(first_part) + 3 < 70:
+                                break
+                            i += 1
+                        filename = '{}...{}'.format(first_part, last_part)
+                    self._view.set_status_label("Data written to: '{}'".format(filename))
+                except Exception as e:
+                    message = 'Could not write the data to the file:\n{}\n{}'.format(filename, e)
+                    with wx.MessageDialog(self._view, message, title, wx.ICON_EXCLAMATION) as dlg:
+                        dlg.ShowModal()
+                    self._view.set_status_label('Error writing data to file')
+                self._view.set_progress(0)
+
     ##################
     # Event handlers #
     ##################
 
     def _on_connect(self, event):
         wx.CallAfter(self._connect)
+        event.Skip()
+
+    def _on_flash_read(self, event):
+        wx.CallAfter(self._read_flash)
         event.Skip()
 
     def _on_view_close(self, event):
